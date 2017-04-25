546ad2726f98552bca58b6a28ef8529fe8bb0501
csingh312 <candice.singh312@gmail.com>
2017-04-24 21:36:37 -0400

    Ready to Commit

--- sample.py
+++ sample.py
@@ -14,6 +14,8 @@ url = 'http://best-books.publishersweekly.com/pw/best-books/2016/fiction'
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'lxml')
 data= soup.find_all("div", {"class":"rule-top"})
+count = 0
 for item in data:
-        print item.contents[1].text, item.contents[3].text.split('(',1)[0], 'Publisher Weekly'
+        count += 1
+        print count, item.contents[1].text, item.contents[3].text.split('(',1)[0], 'Publisher Weekly'
 
